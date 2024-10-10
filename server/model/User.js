54e70925824aa6430bad95344757e8108fafcedc
AhmadI7san22 <166217880+AhmadI7san22@users.noreply.github.com>
2024-10-10 12:50:39 +0300

    Update User.js

--- server/model/User.js
+++ server/model/User.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// User schema
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
+
+// Compare passwords method
+UserSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+
 
 const UserModel = mongoose.model("users", UserSchema);
 
