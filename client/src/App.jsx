98e4cff3567362f2588707f3d2ace3dc3977c7a5
AhmadI7san22 <ahmadehsanbishawi@gmail.com>
2024-10-18 14:28:11 +0300

    deploying the website on render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,31 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from './Signup';
 import Login from './Login';
 import Home from './Home';
-
+import PrivateRoute from './privateRoute';
 
 function App() {
+  const token = localStorage.getItem('authToken');
+
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/register' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route 
+          path='/login' 
+          element={token ? <Navigate to="/home" /> : <Login />} 
+        />
+        <Route 
+          path='/home' 
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          } 
+        />
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
---
+++ client/src/privateRoute.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+
+const PrivateRoute = ({ children }) => {
+  const [isValid, setIsValid] = useState(false);
+  const token = localStorage.getItem('authToken');
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        await axios.get('https://signin-signup-a8q1.onrender.com/verifyToken', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setIsValid(true);
+      } catch (error) {
+        localStorage.removeItem('authToken'); // Remove invalid token
+        setIsValid(false);
+      }
+    };
+
+    if (token) {
+      verifyToken();
+    }
+  }, [token]);
+
+  if (!token || !isValid) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
